292ec176abdcedea9d6733681745b8296e53ea07
au665940 <au665940@post.au.dk>
2021-09-24 15:10:37 +0200

    Deleted unused imports in SettlerActionStrategy - Mads Vejrup

--- src/main/java/hotciv/variants/actionStrategy/SettlerActionStrategy.java
+++ src/main/java/hotciv/variants/actionStrategy/SettlerActionStrategy.java
@@ -2,10 +2,6 @@ package hotciv.variants.actionStrategy;
 
 import hotciv.framework.ExtendedGame;
 import hotciv.framework.Position;
-import hotciv.standard.GameImpl;
-import hotciv.standard.UnitImpl;
-
-import java.util.HashMap;
 
 public interface SettlerActionStrategy {
     void performAction(ExtendedGame game, Position p);
