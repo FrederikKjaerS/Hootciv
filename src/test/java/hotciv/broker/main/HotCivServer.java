cf8af1aea9362c318342d29a9a1c4258b1fd6f8e
au665940 <famxad-fuTmad-nyhfi7>
2021-11-26 10:51:46 +0100

    Not implemented server :(

---
+++ src/test/java/hotciv/broker/main/HotCivServer.java
@@ -0,0 +1,38 @@
+package hotciv.broker.main;
+
+import frds.broker.Invoker;
+import frds.broker.ipc.socket.SocketServerRequestHandler;
+import hotciv.broker.GameInvoker;
+import hotciv.framework.Game;
+import hotciv.stub.StubGameBroker;
+
+public class HotCivServer {
+
+    private static Thread daemon;
+
+    public static void main(String[] args) throws Exception {
+        new HotCivServer(args[0]); // No error handling!
+    }
+
+
+    public HotCivServer(String type) throws Exception {
+        int port = 37321;
+
+        Game gameServant = new StubGameBroker();
+        Invoker invoker = new GameInvoker(gameServant);
+
+        // Configure a socket based server request handler
+        SocketServerRequestHandler ssrh =
+                new SocketServerRequestHandler();
+        ssrh.setPortAndInvoker(port, invoker);
+
+        // Welcome
+        // Welcome
+        System.out.println("=== HotCiv Socket based Server Request Handler (port:"
+                + port + ") ===");
+        System.out.println(" Use ctrl-c to terminate!");
+        ssrh.start();
+
+    }
+}
+
